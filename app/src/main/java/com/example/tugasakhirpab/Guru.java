cf2f7b08fedc6fd069998b83d06cdd737e4aaf8b
kevinganing1 <18523064@students.uii.ac.id>
2020-12-03 11:58:31 +0700

    Tambah Guru Class

---
+++ app/src/main/java/com/example/tugasakhirpab/Guru.java
@@ -0,0 +1,16 @@
+package com.example.tugasakhirpab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Guru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guru);
+
+        // ini adalah guru yang telah dibina
+    }
+}
