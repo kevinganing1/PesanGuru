fdd661396fa1dca4c3ca7fc710859306d4eec3a5
kevinganing1 <18523064@students.uii.ac.id>
2020-12-04 22:51:00 +0700

    Update SDK To 22

--- app/src/main/java/com/example/tugasakhirpab/MenuUtama.java
+++ app/src/main/java/com/example/tugasakhirpab/MenuUtama.java
@@ -3,37 +3,48 @@ package com.example.tugasakhirpab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+import android.content.Intent;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MenuUtama extends AppCompatActivity {
-    Spinner spinner1, spinner2, spinner3;
+public class MenuUtama extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner1, spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_utama);
+        setContentView (R.layout.activity_menu_utama);
         spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
+
         spinner2 = findViewById(R.id.spinner2);
-        spinner3 = findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.Spinner_items2,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(this);
 
-        String[] value = {"Sekolah Menengah Pertama", "Sekolah Menengah Akhir"};
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(value));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.style_spinner_utama,arrayList);
-        spinner1.setAdapter(arrayAdapter);
 
-        String[] value2 = {"1", "2", "3"};
-        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList(value2));
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this,R.layout.style_spinner_utama,arrayList2);
-        spinner2.setAdapter(arrayAdapter2);
 
-        String[] value3 = {"IPA", "Matematika", "IPS"};
-        ArrayList<String> arrayList3 = new ArrayList<>(Arrays.asList(value3));
-        ArrayAdapter<String> arrayAdapter3 = new ArrayAdapter<>(this,R.layout.style_spinner_utama,arrayList3);
-        spinner3.setAdapter(arrayAdapter3);
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
